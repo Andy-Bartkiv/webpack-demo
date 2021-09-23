5a341ba4074b0b1f691eb4e81b4a226d6269e5e3
Andy-W <andy.bartkiv@gmail.com>
2021-09-23 18:12:35 +0300

    asset management complete

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import _ from 'lodash';
+// import myName from './myName';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
     const element = document.createElement('div');
@@ -6,7 +11,21 @@ function component() {
     // --- Lodash, currently included via a script, is required for this line to work
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    element.classList.add('hello');
+
+      // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+    
+    console.log(Data);
+    console.log('TEST - TEST');
+    console.log(Notes);
+
+    // use my function!
+    // element.innerHTML = myName('Andy')
+
     return element;
   }
   
